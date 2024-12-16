f0c91d9b050840af541db556c216f5635decf86d
Faiz Alam <faizalam2432005@gmail.com>
2024-12-16 22:22:52 +0530

    MMKV and zustand seted up

---
+++ src/store/userStore.tsx
@@ -0,0 +1,38 @@
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+import { mmkvStorage } from './storage';
+
+type CustomLocation = {
+  latitude: number;
+  longitude: number;
+  address: string;
+} | null;
+
+interface UserStoreProps {
+  user: any;
+  location: CustomLocation;
+  outOfRange: boolean;
+  setUser: (data: any) => void;
+  setOutOfRange: (data: boolean) => void;
+  setLocation: (data: CustomLocation) => void;
+  clearData: () => void;
+}
+
+export const useUserStorage = create<UserStoreProps>()(
+  persist(
+    (set, get) => ({
+      user: null,
+      location: null,
+      outOfRange: false,
+      setUser: (data) => set({ user: data }),
+      setLocation: (data) => set({ location: data }),
+      setOutOfRange: (data) => set({ outOfRange: data }),
+      clearData: () => set({ user: null, location: null, outOfRange: false }),
+    }),
+    {
+      name: 'user-store', // Key for persisted data
+      partialize: (state) => ({ user: state.user }), // Only persist the user property
+      storage: createJSONStorage(() => mmkvStorage), // Use MMKV for storage
+    }
+  )
+);
